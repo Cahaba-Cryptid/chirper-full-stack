83ba282e7c392236ddf3f44347f8af102d486eff
Hayden <chfrank9011@gmail.com>
2019-05-07 18:57:10 -0500

    added .gitignore files

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,14 +1,6 @@
-import * as mysql from 'mysql';
+import Connection from '../config'
 
 import Chirps from './chirps'
-
-export const Connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'chirprapp',
-    password: 'password123',
-    database: 'chirpr'
-});
 
 export const Query = (query: string, values?:Array<string | number>) => {
     return new Promise<Array<any>>((resolve, reject) => {
